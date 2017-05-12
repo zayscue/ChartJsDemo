676f16ab0e69cddea59fad5505ea7b6c937fb53f
Zackery T. Ayscue <ZAyscue@ecslimited.com>
2017-05-12 08:33:08 -0400

    Added Dynamic Data Example

--- app/index.js
+++ app/index.js
@@ -1,40 +1,51 @@
 var Chart = require('chart.js');
-var ctx = document.getElementById('DemoChart').getContext('2d');
-var demoChart = new Chart(ctx, {
-    type: 'bar',
-    responsive: true,
-    maintainAspectRatio: false,
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 16, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+var _ = require('underscore');
+
+fetch('http://localhost:5000/api/projects-queries/active-per-office').then(function (response) {
+    response.json().then(function (jsonArray) {
+        var labels = _.map(jsonArray, function(item) {
+            return 'Office ' + item['ecsOffice'];
+        });
+        var data = _.map(jsonArray, function(item) {
+            return item['activeProjects'];
+        });
+        var backgroundColors = new Array(jsonArray.length);
+        var borderColors = new Array(jsonArray.length);
+        for(var i = 0; i < jsonArray.length; i++) {
+            var r = Math.floor(Math.random() * 256);
+            var g = Math.floor(Math.random() * 256);
+            var b = Math.floor(Math.random() * 256);
+            var backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+            var borderColor = 'rgb(' + r + ',' + g + ',' + (b + 1) + ')';
+            backgroundColors[i] = backgroundColor;
+            borderColors[i] = borderColor;
         }
-    }
+        var ctx = document.getElementById('DemoChart').getContext('2d');
+        var demoChart = new Chart(ctx, {
+            type: 'bar',
+            responsive: true,
+            xAxisID: 'Ecs Office',
+            yAxisID: 'Number of Active Projects',
+            maintainAspectRatio: false,
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: '# of Active Projects',
+                    data: data,
+                    backgroundColor: backgroundColors,
+                    borderColor: borderColors,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    });
 });
